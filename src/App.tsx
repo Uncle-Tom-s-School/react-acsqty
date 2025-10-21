79f92b6b8eddec61440612a8060f32ef28f8f308
Ács Attila <atika@example.com>
2025-10-21 09:18:27 +0200

    kÉSZ dolgozat

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react"
+import type {Beer} from "./components/BeerCard"
 import BeerCard from "./components/BeerCard"
 
 const App = () => {
+  const [data, setData] = useState<Beer[]>([])
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(res => res.json())
+    .then(kapveg => setData(kapveg))
+},[])
+
+
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {data.map(item => <BeerCard{...item}/>)}
     </div>
   )
 }
